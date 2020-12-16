d6941cd0bc3ab13e3fcd300dc16d30aeae635d79
Incurromunio <75810561+Incurromunio@users.noreply.github.com>
2020-12-16 20:21:56 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function Add() {
+    var firstAdd = prompt("Enter first number to add");
+    var secondAdd = prompt("Enter second number to add");
+    var sumAdd = Number(firstAdd) + Number(secondAdd);
+    alert(sumAdd);
+}
+
+
+function Subtract() {
+    var firstSub = prompt("Enter first number");
+    var secondSub = prompt("Enter number to subtract from the first");
+    var sumSub = Number(firstSub) - Number(secondSub);
+    alert(sumSub);
+}
+
+function Multiplication() {
+    var firstMul = prompt("Enter first number");
+    var secondMul = prompt("Enter second number");
+    var sumMul = Number(firstMul) * Number(secondMul);
+    alert(sumMul);
+}
+
+function Divide() {
+    var firstDiv = prompt("Enter first number");
+    var secondDiv = prompt("Enter number to divide the first by");
+    var sumDiv = Number(firstDiv) / Number(secondDiv);
+    alert(sumDiv);
+}
+
+
